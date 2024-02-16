952dfce6409fb4e02898a17bd3d08c0e4372b807
Chinmay <signups@chinmay.design>
2024-02-16 14:51:15 +0100

    ExpDispatches injected and working. Refresh still issue persists

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import './styles/style.css'
 import pageGlobal from './pages/global'
 import pageClimateImportance from './pages/page_climateImportance'
+import pageExpeditionDispatches from './pages/page_expeditionDispatches'
 import pageHome from './pages/page_home'
 
 // Global
@@ -12,7 +13,7 @@ if (window.location.pathname === '/') {
 } else if (window.location.pathname === '/climate-importance') {
   pageClimateImportance()
 } else if (window.location.pathname === '/expedition-dispatches') {
-  console.log('Expedition Dispatches Page')
+  pageExpeditionDispatches()
 } else if (window.location.pathname === '/journey') {
   console.log('Journey Page')
 } else if (window.location.pathname === '/series') {
