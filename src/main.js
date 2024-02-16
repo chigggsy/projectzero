47b02b4d1d32d377cb772b73a63392ebb2046c17
Chinmay <signups@chinmay.design>
2024-02-16 13:30:53 +0100

    Set up pages for each page on PZ and added installed all dependencies

--- src/main.js
+++ src/main.js
@@ -1,8 +1,5 @@
-import animateTitle from './features/animateTitle'
-import createBadge from './features/createBasge'
+// import animateTitle from './features/animateTitle'
+// import createBadge from './features/createBasge'
 import './styles/style.css'
 
-console.log('Welcome to Vite + JS + Webflow!')
-
-createBadge()
-animateTitle()
+console.log('Testing if the server is working')
